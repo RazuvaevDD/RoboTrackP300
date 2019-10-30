38ace48e502b352d0d5345faf0f89071d8ec338f
RazuvaevDD <Razuvaev_DD@mail.ru>
2019-10-30 17:25:34 +0300

    Added gravity and score

--- GAME/src/GameScene.cpp
+++ GAME/src/GameScene.cpp
@@ -2,11 +2,16 @@
 #include "GameView.h"
 #include <float.h> // Константа FLT_EPSILON
 #include <math.h>
+#include <iostream>
+#include <sstream>
 
 // Добавим часто используемые идентификаторы в глобальное пространство имён.
 using sf::Keyboard;
 using sf::Vector2f;
 using sf::Vector2i;
+using sf::Font;
+using sf::Text;
+using sf::Color;
 
 // Абсолютная скорость движения игрока.
 static const float PLAYER_SPEED = 110;
@@ -37,11 +42,11 @@ static Vector2f GetPlayerDirection()
     if (Keyboard::isKeyPressed(Keyboard::W) || Keyboard::isKeyPressed(Keyboard::Up))
     {
         direction.y = -1;
-    }
-    else if (Keyboard::isKeyPressed(Keyboard::S) || Keyboard::isKeyPressed(Keyboard::Down))
-    {
-        direction.y = +1;
-    }
+	}
+	else {
+		direction.y = 1;
+	}
+    
     if (Keyboard::isKeyPressed(Keyboard::A) || Keyboard::isKeyPressed(Keyboard::Left))
     {
         direction.x = -1;
@@ -54,6 +59,10 @@ static Vector2f GetPlayerDirection()
     return Normalize(direction);
 }
 
+Font font; 
+Text text("", font, 25);
+
+
 GameScene* NewGameScene(const char* file)
 {
     GameScene* pLogic = new GameScene;
@@ -65,15 +74,24 @@ GameScene* NewGameScene(const char* file)
     pLogic->enemies = level.GetAllObjects("enemy");
 	pLogic->blocks = level.GetAllObjects("");
 
+	
+	font.loadFromFile("res/arial.ttf");
+
+	//text.setColor(Color::Red);
+	text.setStyle(sf::Text::Bold);
+
     return pLogic;
 }
 
+int SCORE = 0;
 // ТУТ ЛОГИКА СТОЛКНОВЕНИЙ
 void UpdateGameScene(void *pData, GameView &view, float deltaSec)
 {
     // Извлекаем указатель на GameLogic, ранее переданный в игровой цикл.
     GameScene *pLogic = reinterpret_cast<GameScene *>(pData);
     (void)deltaSec;
+
+	bool Gravity = true;
 
     TmxObject &player = pLogic->player;
     const Vector2f movement = Round(GetPlayerDirection() * PLAYER_SPEED * deltaSec);
@@ -83,19 +101,46 @@ void UpdateGameScene(void *pData, GameView &view, float deltaSec)
 	for (int i = 0; i < pLogic->coins.size(); i++) {
 
 		if (pLogic->coins[i].rect.intersects(player.rect)) {
-			pLogic->coins[i].show = false;
+			if (pLogic->coins[i].show) {
+				pLogic->coins[i].show = false;
+				SCORE += 10;
+			}
 		}
-		else {
+	}
+
+	for (int i = 0; i < pLogic->enemies.size(); i++) {
+
+		if (pLogic->enemies[i].rect.intersects(player.rect)) {
+			if (pLogic->enemies[i].show) {
+				pLogic->enemies[i].show = false;
+				SCORE -= 20;
+			}
 		}
 	}
 
 	for (int i = 0; i < pLogic->blocks.size(); i++)
 	{
 		if (pLogic->blocks[i].rect.intersects(player.rect)) {
-			player.MoveBy(Vector2f(-movement.x, -movement.y));
+			if (pLogic->blocks[i].show) {
+				Gravity = false;
+				player.MoveBy(Vector2f(-movement.x, -movement.y));
+				break;
+			}
 		}
 	}
 
+	if (!Gravity) {
+		player.MoveBy(Vector2f(movement.x, 0));
+		for (int i = 0; i < pLogic->blocks.size(); i++)
+		{
+			if (pLogic->blocks[i].rect.intersects(player.rect)) {
+				player.MoveBy(Vector2f(-movement.x, 0));
+				break;
+			}
+		}
+	}
+	
+	
 
     const Vector2i windowSize = view.windowSize;
     SetCameraCenter(view, player.sprite.getPosition() + Vector2f(windowSize.x / 4, windowSize.y / 4));
@@ -125,6 +170,16 @@ void DrawGameScene(void *pData, GameView &view)
 	}
 
     target.draw(pLogic->player.sprite);
+	
+	
+	std::ostringstream oss;
+	oss << "Score: " << SCORE;
+
+	text.setString(oss.str());
+	text.setPosition(view.camera.getCenter().x - 393, view.camera.getCenter().y - 300);//348, 369
+
+	std::cout << view.camera.getCenter().x << " " << view.camera.getCenter().y << "\n";
+	view.window.draw(text);
 }
 
 void DestroyGameScene(GameScene *&pScene)
