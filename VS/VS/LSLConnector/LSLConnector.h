0c0075eabafd31a1b4d8c959d72693523493df37
Neizhko <v.v.neizhko@gmail.com>
2019-11-21 15:44:15 +0300

    Added LSL listener and data log.

--- VS/VS/LSLConnector/LSLConnector.h
+++ VS/VS/LSLConnector/LSLConnector.h
@@ -1,13 +1,21 @@
 #pragma once
 
-#include <boost/signals2.hpp>
+//#include <boost/signals2.hpp>
+#include <fstream>
+#include "../../LSLlibs&headers/lsl_cpp.h"
 
 class LSLConnector
 {
 public:
 	LSLConnector();
-	void getData();
+	std::vector<float> getData();
 	~LSLConnector();
-	boost::signals2::signal<void()> sendData; 
+	//boost::signals2::signal<void()> sendData; 
+private:
+	std::string field = "type", value = "EEG";
+	std::vector<lsl::stream_info> results = lsl::resolve_stream(field, value);
+	lsl::stream_inlet inlet;
+	std::vector<float> sample;
+	std::ofstream out;
 };
 
